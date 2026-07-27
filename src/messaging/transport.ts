4057d17c31e669765f89bca1896d104f707b9b75
Matthew Buckton <matthew@buckton.org>
2026-07-27 17:13:50 +1000

    Add generic event publishing to transports

--- src/messaging/transport.ts
+++ src/messaging/transport.ts
@@ -5,4 +5,5 @@ export interface MessageTransport {
   disconnect(): Promise<void>;
   publishTask(task: DroneTask): Promise<void>;
   cancelTask(task: DroneTask): Promise<void>;
+  publishEvent(destination: string, payload: unknown): Promise<void>;
 }
